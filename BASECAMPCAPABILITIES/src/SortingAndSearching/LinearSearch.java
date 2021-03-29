73987af610fe4f95710e8c7eec80b8c4b066fd1c
Richa Pandey <richapandey0297@gmail.com>
2021-03-29 10:06:38 +0530

    upload bascecamp

---
+++ BASECAMPCAPABILITIES/src/SortingAndSearching/LinearSearch.java
@@ -0,0 +1,38 @@
+
+
+
+package SortingAndSearching;
+import java.util.Scanner;
+public class LinearSearch {
+ public int linearSearch(int[] arr,int element)
+ {
+	 int position=0;
+	 for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==element)
+			{
+				position=i;
+			}
+		}
+	 return position;
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of an array");
+		int size=sc.nextInt();
+		int[] arr=new int[size];
+		System.out.println("Enter the elements in array");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Enter an elemet to be searched");
+		int element=sc.nextInt();
+		LinearSearch obj=new LinearSearch();
+		int result=obj.linearSearch(arr, element);
+		System.out.println("element found at index" +result);
+
+	}
+
+}
